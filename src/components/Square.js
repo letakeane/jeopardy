4ed066e649419d7d6431929636ec853088315327
Leta Keane <leta@hey.com>
2023-10-27 09:05:09 -0600

    Only show each question once

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,26 +1,38 @@
-import { useState } from 'react'
-import questions from '../questions.js'
-import './Square.css'
+import { useState, useEffect } from "react";
+import questions from "../questions.js";
+import "./Square.css";
 
-function Square({value, type}) {
+function Square({ value, type }) {
   const [showQuestion, setShowQuestion] = useState(false);
+  const [currQuestion, setCurrQuestion] = useState("");
 
   function onClick() {
     setShowQuestion(!showQuestion);
+
+    if (showQuestion === false) {
+      setCurrQuestion(questions[type][value].pop());
+    }
   }
 
+  useEffect(() => {
+    setCurrQuestion(questions[type][value].pop());
+  }, "");
+
   return (
-    <section className={`square ${showQuestion ? 'highlight' : ''}`} onClick={onClick}>
-      {!showQuestion &&
-        <h3>{value}</h3>
-      }
-      {showQuestion &&
-      <>
-        <p>{questions[type][value][Math.floor(Math.random()*questions[type][value].length)]}</p>
-      </>
-      }
+    <section
+      className={`square ${showQuestion ? "highlight" : ""} ${
+        currQuestion ? "" : "empty"
+      }`}
+      onClick={currQuestion ? onClick : null}
+    >
+      {!showQuestion && <h3>{value}</h3>}
+      {showQuestion && (
+        <>
+          <p>{currQuestion}</p>
+        </>
+      )}
     </section>
-  )
+  );
 }
 
 export default Square;
